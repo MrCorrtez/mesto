fe858e297a84903bb97674fd0b2595b0135cb22d
Vitaly M <mezhakov@gmail.com>
2022-02-28 23:37:01 +0300

    modify popup, add interactive logic

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,33 @@
 let editButton = document.querySelector('.profile__editButton');
+let exitButton = document.querySelector('.popup__exitButton');
+let saveButton = document.querySelector('.popup__saveButton');
+
+let popup = document.querySelector('.popup');
+
+let inputName = document.querySelector('.popup__name');
+let inputOccupation = document.querySelector('.popup__occupation');
+
+let profileName = document.querySelector('.profile__name');
+let profileOccupation = document.querySelector('.profile__occupation');
 
 function editProfile() {
-  alert(document.querySelector('.elements__like').getAttribute('background-image'));
+  popup.classList.add('popup_opened');
+  inputName.value = profileName.textContent;
+  inputOccupation.value = profileOccupation.textContent;
+}
+
+function exitProfile() {
+  popup.classList.remove('popup_opened');
+}
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileName.textContent = inputName.value;
+  profileOccupation.textContent = inputOccupation.value;
+  exitProfile();
 }
 
 editButton.addEventListener('click', editProfile);
+exitButton.addEventListener('click', exitProfile);
+saveButton.addEventListener('click', saveProfile);
+popup.addEventListener('submit', formSubmitHandler);

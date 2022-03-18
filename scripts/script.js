f43935c514bb19c95af759420db8796a1aae0094
Vitaly M <mezhakov@gmail.com>
2022-03-18 23:36:30 +0300

    add big card, js refactoring

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,10 @@
 const editButton = document.querySelector('.profile__editButton');
-const exitButton = document.querySelector('.popup__exitButton');
 const saveButton = document.querySelector('.popup__saveButton');
+const exitButton = document.querySelectorAll('.exitButton');
 
-const popup      =  document.querySelector('.popup');
+const popup        = document.querySelector('.popup');
+const popupBigCard = document.querySelector('.popup__big-card');
+const popupProfile = document.querySelector('.popup__container');
 
 const inputName       = document.querySelector('.popup__name');
 const inputOccupation = document.querySelector('.popup__occupation');
@@ -39,29 +41,41 @@ const initialCards = [
   }
 ];
 
-const editProfile = () => {
+const openProfile = () => {
 
-  popup.classList.add('popup_opened');
+  popup.classList.remove('hidden');
+  popupProfile.classList.remove('hidden');
 
   inputName.value       = profileName.textContent;
   inputOccupation.value = profileOccupation.textContent;
 
 }
 
-const exitProfile = () => {
+const exitPopup = evt => {
 
-  popup.classList.remove('popup_opened');
+  evt.target.closest('form').classList.add('hidden');
+  popup.classList.add('hidden');
 
 }
 
-const formSubmitHandler = event => {
+const formSubmitHandler = evt => {
 
-  event.preventDefault();
+  evt.preventDefault();
 
   profileName.textContent       = inputName.value;
   profileOccupation.textContent = inputOccupation.value;
 
-  exitProfile();
+  exitPopup(evt);
+
+}
+
+const openImagePopup = evt => {
+
+  const bigCardImage = document.querySelector('.popup__image')
+
+  popup.classList.remove('hidden');
+  popupBigCard.classList.remove('hidden');
+  bigCardImage.src = evt.target.src;
 
 }
 
@@ -71,10 +85,12 @@ const addCard = element => {
   const cardElement  = cardTemplate.querySelector('.elements__element').cloneNode(true);
 
   cardElement.querySelector('.elements__name').textContent = element.name;
+  cardElement.querySelector('.elements__name').alt = element.name;
   cardElement.querySelector('.elements__image').src = element.link;
 
-  cardElement.querySelector('.elements__like').addEventListener('click', event => event.target.classList.toggle('elements__like_active'));
-  cardElement.querySelector('.elements__deleteButton').addEventListener('click', event => event.target.closest('.elements__element').remove());
+  cardElement.querySelector('.elements__like').addEventListener('click', evt => evt.target.classList.toggle('elements__like_active'));
+  cardElement.querySelector('.elements__deleteButton').addEventListener('click', evt => evt.target.closest('.elements__element').remove());
+  cardElement.querySelector('.elements__image').addEventListener('click', openImagePopup);
 
   return cardElement;
 
@@ -89,6 +105,6 @@ const initialRender = () => {
 
 initialRender();
 
-editButton.addEventListener('click', editProfile);
-exitButton.addEventListener('click', exitProfile);
-popup.addEventListener('submit', formSubmitHandler);
+exitButton.forEach(element => element.addEventListener('click', exitPopup));
+editButton.addEventListener('click', openProfile);
+popupProfile.addEventListener('submit', formSubmitHandler);

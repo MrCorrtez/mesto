c063fe7e71a2c2408f52e24d1b1f9a127c3fe901
Vitaly M <mezhakov@gmail.com>
2022-03-19 14:50:50 +0300

    add big card, js refactoring

--- scripts/script.js
+++ scripts/script.js
@@ -1,96 +1,109 @@
-const editButton = document.querySelector('.profile__editButton');
-const saveButton = document.querySelector('.popup__saveButton');
-const exitButton = document.querySelectorAll('.exitButton');
+const editButton  = document.querySelector('.profile__editButton');
+const addButton   = document.querySelector('.profile__addButton');
+const saveButton  = document.querySelector('.popup__saveButton');
+const exitButtons = document.querySelectorAll('.exitButton');
 
-const popup        = document.querySelector('.popup');
-const popupBigCard = document.querySelector('.popup__big-card');
-const popupProfile = document.querySelector('.popup__container');
+const popup       = document.querySelector('.popup');
+const formBigCard = document.querySelector('.popup__big-card');
+const formProfile = document.querySelector('.type_profile');
+const formNewItem = document.querySelector('.type_new-item');
 
-const inputName       = document.querySelector('.popup__name');
-const inputOccupation = document.querySelector('.popup__occupation');
+const inputName        = document.querySelector('.type_profile .popup__name');
+const inputOccupation  = document.querySelector('.type_profile .popup__occupation');
+const inputDescription = document.querySelector('.type_new-item .popup__name');
+const inputLink        = document.querySelector('.type_new-item .popup__occupation');
 
 const profileName       = document.querySelector('.profile__name');
 const profileOccupation = document.querySelector('.profile__occupation');
 
-const elements = document.querySelector('.elements');
+const bigCardImage   = document.querySelector('.popup__image')
+const bigCardCaption = document.querySelector('.popup__caption')
 
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
+const cardTemplate = document.querySelector('#card-template').content;
+const cardTemplateNode = cardTemplate.querySelector('.elements__element');
 
-const openProfile = () => {
+const cardElements = document.querySelector('.elements');
 
-  popup.classList.remove('hidden');
-  popupProfile.classList.remove('hidden');
+const changeFormVisibility = form => {
+
+  popup.classList.toggle('hidden');
+  popup.classList.toggle('visible');
+  form.classList.toggle('hidden');
+  form.classList.toggle('visible');
+
+}
+
+const openProfileForm = () => {
 
   inputName.value       = profileName.textContent;
   inputOccupation.value = profileOccupation.textContent;
 
-}
-
-const exitPopup = evt => {
-
-  evt.target.closest('form').classList.add('hidden');
-  popup.classList.add('hidden');
+  changeFormVisibility(formProfile);
 
 }
 
-const formSubmitHandler = evt => {
+const openNewItemForm = () => {
+
+  changeFormVisibility(formNewItem);
+
+}
+
+const openImageForm = evt => {
+
+  bigCardImage.src           = evt.target.src;
+  bigCardCaption.textContent = evt.target.alt;
+
+  changeFormVisibility(formBigCard);
+
+}
+
+const exitForm = evt => {
+
+  const evtForm = evt.target.closest('form');
+
+  changeFormVisibility(evtForm);
+
+}
+
+const formProfileSubmitHandler = evt => {
 
   evt.preventDefault();
 
   profileName.textContent       = inputName.value;
   profileOccupation.textContent = inputOccupation.value;
 
-  exitPopup(evt);
+  exitForm(evt);
 
 }
 
-const openImagePopup = evt => {
+const formNewItemSubmitHandler = evt => {
 
-  const bigCardImage = document.querySelector('.popup__image')
+  evt.preventDefault();
 
-  popup.classList.remove('hidden');
-  popupBigCard.classList.remove('hidden');
-  bigCardImage.src = evt.target.src;
+  if (cardElements.children.length === 6) {
+    cardElements.lastElementChild.remove();
+  }
+
+  const newElement = {name: inputDescription.value, link: inputLink.value};
+  const newCard    = addCard(newElement);
+
+  cardElements.prepend(newCard);
+
+  exitForm(evt);
 
 }
 
 const addCard = element => {
 
-  const cardTemplate = document.querySelector('#card-template').content;
-  const cardElement  = cardTemplate.querySelector('.elements__element').cloneNode(true);
+  const cardElement  = cardTemplateNode.cloneNode(true);
 
   cardElement.querySelector('.elements__name').textContent = element.name;
-  cardElement.querySelector('.elements__name').alt = element.name;
-  cardElement.querySelector('.elements__image').src = element.link;
+  cardElement.querySelector('.elements__image').src        = element.link;
+  cardElement.querySelector('.elements__image').alt         = element.name;
 
   cardElement.querySelector('.elements__like').addEventListener('click', evt => evt.target.classList.toggle('elements__like_active'));
   cardElement.querySelector('.elements__deleteButton').addEventListener('click', evt => evt.target.closest('.elements__element').remove());
-  cardElement.querySelector('.elements__image').addEventListener('click', openImagePopup);
+  cardElement.querySelector('.elements__image').addEventListener('click', openImageForm);
 
   return cardElement;
 
@@ -98,13 +111,42 @@ const addCard = element => {
 
 const initialRender = () => {
 
+  const initialCards = [
+    {
+      name: 'Лондон',
+      link: 'https://s3.nat-geo.ru/images/2019/4/10/b2580431d1fb410fa57d94a4c97a9213.max-1200x800.jpg'
+    },
+    {
+      name: 'Канада',
+      link: 'https://a.d-cd.net/fe84fbes-1920.jpg'
+    },
+    {
+      name: 'Швейцария',
+      link: 'https://dnpmag.com/wp-content/uploads/2016/06/718.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Тайланд',
+      link: 'https://tourdv.ru/wp-content/uploads/2018/04/Tajland.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
   const initialElements = initialCards.map(addCard);
-  initialElements.forEach(element => elements.append(element));
+  initialElements.forEach(element => cardElements.append(element));
 
 }
 
 initialRender();
 
-exitButton.forEach(element => element.addEventListener('click', exitPopup));
-editButton.addEventListener('click', openProfile);
-popupProfile.addEventListener('submit', formSubmitHandler);
+exitButtons.forEach(element => element.addEventListener('click', exitForm));
+editButton.addEventListener('click', openProfileForm);
+addButton.addEventListener('click', openNewItemForm);
+formProfile.addEventListener('submit', formProfileSubmitHandler);
+formNewItem.addEventListener('submit', formNewItemSubmitHandler);
